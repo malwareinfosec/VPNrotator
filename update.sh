d0054bfdd6c1beaca91579e3f938021351ef1f9b
Jérôme Segura <malwareinfosec@gmail.com>
2021-12-22 13:35:05 -0800

    updated to version 2.5

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ latestversion=$(curl https://raw.githubusercontent.com/malwareinfosec/VPNrotator
 # Download latest core files
 declare -a CoreFiles=("VPN.sh" "countries.txt" "dn.sh" "up.sh" "vpnservice.sh" )
 for val in ${CoreFiles[@]}; do
-   curl https://github.com/malwareinfosec/VPNrotator/blob/master/$val --output $val
+   curl https://raw.githubusercontent.com/malwareinfosec/VPNrotator/master/$val --output $val
 done
 echo $latestversion > version.info
 echo "Updated VPN Rotator to version: $latestversion"
